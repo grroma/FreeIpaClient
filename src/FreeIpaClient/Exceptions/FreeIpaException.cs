4e5ea3ac60233554f155a5790d73a898ee7a93fb
Роман Гришин <rgrishin@htsts.ru>
2021-05-07 11:47:45 +0300

    public access modifiers FreeIpaException

--- src/FreeIpaClient/Exceptions/FreeIpaException.cs
+++ src/FreeIpaClient/Exceptions/FreeIpaException.cs
@@ -5,15 +5,15 @@ namespace FreeIpaClient.Exceptions
 {
     public class FreeIpaException : Exception
     {
-        private const int InvalidCredential = 2100;        
-        private const int NotFound = 4001;
-        private const int AlreadyExist = 4002;
-        private const int AlreadyActive = 4009;
-        private const int AlreadyInactive = 4010;        
-        private const int NoMatchingEntryFound = 4031;
-        private const int NoModifications = 4202;
+        public const int InvalidCredential = 2100;        
+        public const int NotFound = 4001;
+        public const int AlreadyExist = 4002;
+        public const int AlreadyActive = 4009;
+        public const int AlreadyInactive = 4010;        
+        public const int NoMatchingEntryFound = 4031;
+        public const int NoModifications = 4202;
 
-        private FreeIpaError Error { get; }
+        public FreeIpaError Error { get; }
         public FreeIpaException(string message, HttpStatusCode statusCode, FreeIpaError error = null)
             : base(message)
         {
